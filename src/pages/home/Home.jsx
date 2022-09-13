58e76c566e583ac8c68a6fa0b322f8dbabf276ca
Achref <medachrefg@gmail.com>
2022-09-13 12:52:53 +0100

    Adding widget table and widget of members&style it

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,8 @@ import Chart from '../../components/chart/Chart';
 import FeatureInfo from '../../components/featureInfo/FeatureInfo';
 import { userData } from '../../DummyData';
 import './Home.css';
-
+import WidgetSm from '../../components/widgetSm/WidgetSm';
+import WidgetLg from '../../components/widgetLg/WidgetLg';
 const Home = () => {
 	return (
 		<>
@@ -15,6 +16,10 @@ const Home = () => {
 					datakey={userData.Active_User}
 					grid
 				/>
+				<div className='homeWidget'>
+					<WidgetSm />
+					<WidgetLg />
+				</div>
 			</div>
 		</>
 	);
